e30e8ac3781a9b189fe4fdab9c262af989f0c5df
Tom Ratcliffe <tomratcliffe@uk.ibm.com>
2019-03-05 23:24:19 +0000

    Add oh-my-zsh installation

--- install-tools.exclude.sh
+++ install-tools.exclude.sh
@@ -23,6 +23,8 @@ function installTools() {
 
     brew install zsh
 
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
     git clone https://github.com/lukechilds/zsh-better-npm-completion.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-better-npm-completion
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
