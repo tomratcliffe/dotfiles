a628eb2e43cda321705805a392711ee8b632cf3a
Tom Ratcliffe <tomratcliffe@uk.ibm.com>
2019-04-11 18:14:46 +0100

    Add tpm to install script

--- install-tools.exclude.sh
+++ install-tools.exclude.sh
@@ -21,6 +21,8 @@ function installTools() {
 
     brew install tmux
 
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
     brew install zsh
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
