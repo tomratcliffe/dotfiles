24f64666f910d3dd1bbe15f36eefb6b2e485a484
Tom Ratcliffe <tom.ratcliffe@congenica.com>
2021-05-18 15:40:04 +0100

    Add Congenica work specific aliases

---
+++ work-specific/congenica.sh
@@ -0,0 +1,2 @@
+alias docker-login="aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 144563655722.dkr.ecr.eu-west-1.amazonaws.com"
+alias gcd="git checkout dev"
